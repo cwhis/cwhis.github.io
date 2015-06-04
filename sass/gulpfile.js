418fe674ed47f1baecd7cd6a07d8128cd6be564f
cwhis <caiwenhua1990@163.com>
2015-06-04 15:14:33 +0800

    sass编译

---
+++ sass/gulpfile.js
@@ -0,0 +1,23 @@
+//gulp 运行
+//gulp运行具体一个任务 gulp 任务名称
+
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var sass = require('gulp-sass');
+var	del = require('del');
+
+//创建任务
+gulp.task('sass', function() {
+    gulp.src('sass/test.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+gulp.task('watch',function(){
+	gulp.watch('sass/test.scss', function(){
+	    gulp.run('sass');
+	});
+})
+gulp.task('default', function(){
+    gulp.run('sass','watch');
+});
+
